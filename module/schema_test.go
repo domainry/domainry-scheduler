dea3e954fb0e8ae0153c5285dfce9d751026e34e
panbin <panbin@codeck.ai>
2026-09-24 02:53:27 +0800

    restore scheduler ownership contract test

--- module/schema_test.go
+++ module/schema_test.go
@@ -1,9 +1,22 @@
 package module
 
 import (
+	"slices"
 	"strings"
 	"testing"
+
+	"github.com/domainry/domainry-foundation/schemaownership"
 )
+
+func TestModulePublishesSchedulerOwnedSchema(t *testing.T) {
+	tables := SchemaOwnership()
+	if err := schemaownership.ValidateAll(tables); err != nil {
+		t.Fatal(err)
+	}
+	if len(tables) != 2 || !slices.Equal(OwnedTables(), schemaownership.Names(tables)) {
+		t.Fatalf("Scheduler schema ownership=%d tables=%v", len(tables), OwnedTables())
+	}
+}
 
 func TestModulePublishesCanonicalSchedulerMigrations(t *testing.T) {
 	migrations, err := SchemaMigrations("sqlite", "")
